5cc52f89137c761a6f905bb77a899a411b0939b7
Suraj <surajgautam1111@gmai;l.com>
2023-06-09 10:16:43 +0545

    Input field added with new component

--- Project Food Order App/src/App.js
+++ Project Food Order App/src/App.js
@@ -1,9 +1,18 @@
+import { Fragment } from "react";
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+
 function App() {
   return (
-    <div className="App">
+<Fragment>
+<Header/>
+<main>
+  <Meals/>
+</main>
+</Fragment>
       
-    </div>
+   
   );
 }
-
+ 
 export default App;
